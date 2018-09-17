e507a5221e0fa79c03be8592576bb8da16676f65
uyong11 <uyong@nate.com>
2018-09-17 22:58:40 +0900

    1

--- app/src/main/java/com/cordoba/mytestapp/NewActivity.java
+++ app/src/main/java/com/cordoba/mytestapp/NewActivity.java
@@ -3,6 +3,10 @@ package com.cordoba.mytestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewActivity extends AppCompatActivity {
 
@@ -12,5 +16,11 @@ public class NewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    public void addNew(View view){
+        EditText newItem= (EditText) findViewById(R.id.txtNewItem);
+        String txtItem = newItem.getText().toString();
+        Toast.makeText(getApplicationContext(),  txtItem, Toast.LENGTH_SHORT).show();
     }
 }
